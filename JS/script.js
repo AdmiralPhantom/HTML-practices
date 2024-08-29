638b9655ac5add01b2d4f74cac0e7550ec1d2ec4
AdmiralPhantom <phantompuppetgaming@gmail.com>
2024-08-29 19:44:05 +0300

    projects

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+// data types
+// numbers
+let x = 10;
+console.log(x);
+var y = "hello";
+console.log(y);
+const v = true;
+console/log(v);
+let a ; //undefined
+console.log(a);
+let m = null;
+console.log(m)
